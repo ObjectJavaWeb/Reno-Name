a4cf63d4d7a1bb4e747d7844241451cfd5d5f631
TC1016 <TC1016@192.168.0.1>
2017-04-18 21:44:53 +0800

    实现了回复评论功能

--- WebMusic/src/org/music/factory/DAOFactory.java
+++ WebMusic/src/org/music/factory/DAOFactory.java
@@ -3,10 +3,12 @@ package org.music.factory;
 import org.music.dao.ICommentDAO;
 import org.music.dao.IMymusicDAO;
 import org.music.dao.IQuestion;
+import org.music.dao.IReplyDAO;
 import org.music.dao.IUserDAO;
 import org.music.dao.impl.CommentDAOImpl;
 import org.music.dao.impl.MyMusicDAOImpl;
 import org.music.dao.impl.QuestionDAOImpl;
+import org.music.dao.impl.ReplyDAOImpl;
 import org.music.dao.impl.UserDAOImpl;
 
 public class DAOFactory {
@@ -27,6 +29,9 @@ public class DAOFactory {
 
 	public static IQuestion getIQuestionInstance(){
 		return new QuestionDAOImpl();
+	}
+	public static IReplyDAO getIReplyInstance(){
+		return new ReplyDAOImpl();
 	}
 
 }
